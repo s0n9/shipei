f1d2b0b0c553549f2850d1fa3cc561bc1b57d351
s0n9 <1098781982@qq.com>
2017-10-20 10:52:25 +0800

    update the mainactivity

--- src/main/java/com/example/car/shipei/MainActivity.java
+++ src/main/java/com/example/car/shipei/MainActivity.java
@@ -10,10 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //hello
-        //haha
-        //hehe
-        //lala
         //来自github上的修改
         /**
          *.apk
